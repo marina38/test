de91e08f5e37545a5f60675679ce87deb73f6fc9
Ivan <example@gmail.com>
2019-11-27 17:44:20 +0300

    Это наш первый комит

---
+++ Новая папка/script.js
@@ -0,0 +1,49 @@
+//'use strict';
+a = 5;
+console.log(a);
+
+var leftRight =1;
+
+	let second= 2;
+
+const pi=3.14;
+
+console.log(second);
+console.log(4/0);
+console.log('string'*9);
+let something;
+console.log(something);
+
+let person = {
+	name:'John',
+	age:25,
+	isMarried:false
+}
+//console.log(person['name']);
+let ar=['plum.png', 'orange.jpg'];
+//console.log(ar[1]);
+
+//alert('hello');
+
+//let answer=+prompt('Are you?', 'yes');
+
+//console.log(typeof(answer));
+
+//console.log(4+ +"-object")
+
+let incr=10,
+	decr=10;
+	
+	
+	console.log(incr++);
+	console.log(decr--);
+
+	console.log(5%2);
+	console.log('2'===2);
+
+let isChecked = false;
+    idClose = false;
+
+	console.log(isChecked || !idClose);
+
+
